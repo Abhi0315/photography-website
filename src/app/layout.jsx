3ef734a3a073b62ea6b8ae98f7f1776aba52d930
Abhiram Varier <abhiramv1551@gmail.com>
2025-09-20 14:38:25 +0530

    `Removed font imports and variables from layout.jsx`

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,4 @@
-import { Lato, Playfair_Display } from "next/font/google";
 import "@/app/globals.css";
-
-const lato = Lato({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
-  variable: "--font-lato",
-});
-
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-playfair-display",
-});
 
 export const metadata = {
   title: "Photography Portfolio",
@@ -21,11 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${lato.variable} ${playfair.variable} antialiased bg-white text-black`}
-      >
-        {children}
-      </body>
+      <body className="antialiased bg-white text-black">{children}</body>
     </html>
   );
 }
