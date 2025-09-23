041bc1731cb9d63de5742e0229ea552a203b6b53
Abhiram Varier <abhiramv1551@gmail.com>
2025-09-23 12:02:20 +0530

    `Update next.config.mjs to include image domain configuration`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lensbackend.networkindia.com"],
+  },
+};
 
 export default nextConfig;
